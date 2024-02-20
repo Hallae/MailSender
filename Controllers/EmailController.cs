052628569bf4e2e634ae45f0e051b0145214af49
Hallae <mr.tabby@hotmail.com>
2024-02-20 20:32:24 +0300

    Add project files.

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,32 @@
+﻿using MailKit.Security;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using System.Net.Mail;
+
+namespace AutoMailSender.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult SendEmail(string body)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse("edwin.shields18@ethereal.email"));
+            email.To.Add(MailboxAddress.Parse("edwin.shields18@ethereal.email"));
+            email.Subject = "Test Email Subject";
+            email.Body= new TextPart(MimeKit.Text.TextFormat.Html) { Text = body };
+
+            using var smtp = new SmtpClient();
+            smtp.Connect("smtp.ethereal.email", 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate("edwin.shields18@ethereal.email", "e1k2xtEueZfsAUhUjt");
+            smtp.Send(email);
+            smtp.Disconnect(true);
+
+            return Ok();
+
+        }
+    }
+}

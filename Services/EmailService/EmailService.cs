974caf65b6b7e91e30130717e88de341733dd013
Hallae <mr.tabby@hotmail.com>
2024-02-21 15:25:26 +0300

    email service implementation completed

--- Services/EmailService/EmailService.cs
+++ Services/EmailService/EmailService.cs
@@ -1,12 +1,34 @@
 ﻿using AutoMailSender.Models;
+using MailKit.Security;
+using MimeKit;
+using MailKit.Net.Smtp;
 
 namespace AutoMailSender.Services.EmailService
 {
     public class EmailService : IEmailService
     {
+
+        private readonly IConfiguration _config;
+        
+        public EmailService(IConfiguration config)
+        {
+            _config = config;
+        }
         public void SendEmail(EmailDto request)
         {
-            throw new NotImplementedException();
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse("edwin.shields18@ethereal.email"));
+            email.To.Add(MailboxAddress.Parse(request.Recepients));
+            email.Subject = request.Subject;
+            email.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = request.Body };
+
+            using var smtp = new SmtpClient();
+            smtp.Connect(_config.GetSection("EmailHost").Value, 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_config.GetSection("EmailUserName").Value, _config.GetSection("EmailPassword").Value);
+            smtp.Send(email);
+            smtp.Disconnect(true);
+
+          
         }
     }
 }
